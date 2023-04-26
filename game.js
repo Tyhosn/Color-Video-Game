c55964f65e3fcb72edc3905c00a8bff0a43521dd
Ty Hosn <Abolsonm3@gmail.com>
2023-04-26 16:20:24 -0700

    Commented in, for an easier way to understand

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
+
+//declare a var that takes in array of colors
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+//declare a var that tkaes in an empty array 
 var gamePattern = [];
-var userClickedPattern = [];
 
+//declare a var that tkaes in an empty array 
+var userClickedPattern = [];
+//set a variable to false so the game wont start right away and only on trigger events 
 var started = false;
+
+//declare a var and assign it to letter with a value of 0 
 var level = 0;
 
+//Whenever the key is pressed the game starts changing from false to true and starting the sequence 
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
@@ -31,15 +39,15 @@ $(".btn").click(function() {
 //Created a new function called checkAnswer(), it should take one input with the name currentLevel
 function checkAnswer(currentLevel) {
 
-    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    //Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
       console.log("success");
 
-      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      //If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
       if (userClickedPattern.length === gamePattern.length){
 
-        //5. Call nextSequence() after a 1000 millisecond delay.
+        //Call nextSequence() after a 1000 millisecond delay.
         setTimeout(function () {
           nextSequence();
         }, 1000);
@@ -47,7 +55,7 @@ function checkAnswer(currentLevel) {
       }
 
     } else {
-
+//if you get something wrong this gets called 
       console.log("wrong");
 
       playSound("wrong");
@@ -70,13 +78,14 @@ function checkAnswer(currentLevel) {
 
 function nextSequence() {
 
-  //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+  //Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
   userClickedPattern = [];
 
   level++;
   $("#level-title").text("Level " + level);
-
+//declare a var called randomNUmber that creates a random number up to 3 
   var randomNumber = Math.floor(Math.random() * 4);
+  
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
@@ -84,11 +93,13 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
+//create a function playSound that can be used to play a sound when each option is clicked 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
+//Let an animation happen when the currentcolor is called up with a timer of 100
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
@@ -96,10 +107,10 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-
+//if you get it wrong this gets called and the game restarts  
 function startOver() {
 
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+  //Inside this function, you'll need to reset the values of level, gamePattern and started variables.
   level = 0;
   gamePattern = [];
   started = false;
